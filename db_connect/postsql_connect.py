e37b81b562352e0d2f7ecec1e3e7221c12b91ae4
xiongyang <649384645@qq.com>
2019-10-12 15:04:59 +0800

    a simple example and a tns example to connect postsql

--- db_connect/postsql_connect.py
+++ db_connect/postsql_connect.py
@@ -8,7 +8,7 @@ simple example
 
 def simple_example():
     db_informations = {'ip': 'xxx.xxx.xxx.xxx', 'username': 'mdmp', 'passwd': 'mdmp', 'port': 5432, 'db': 'mdmp'}
-    ''' pymysql '''
+    ''' psycopg2 '''
     conn = psycopg2.connect(host=db_informations['ip'], user =db_informations['username'], password =db_informations['passwd'], database =db_informations['db'],port=db_informations['port'])
     # conn.close()
     ''' sqlalchemy '''
