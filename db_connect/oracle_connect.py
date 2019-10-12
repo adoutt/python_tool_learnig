371feeb5b3d9435440210f9fe88947319efa0d41
xiongyang <649384645@qq.com>
2019-10-12 15:06:11 +0800

    a simple example and a tns example to connect  oracle

--- db_connect/oracle_connect.py
+++ db_connect/oracle_connect.py
@@ -20,12 +20,12 @@ tns example
 def tns_services():
     db_informations = {'ip': 'xxx.xxx.xxx.xxx', 'username': 'yiwu', 'passwd': 'yiwu', 'port': 1521, 'orcl': 'ora11g'}
     oracle_tns = cx.makedsn(db_informations['ip'],db_informations['port'],db_informations['orcl'])
-    '''cx_oracle connect'''
+    '''cx_oracle connect  '''
     conn = cx.connect(db_informations['username'],db_informations['passwd'],oracle_tns)
-    '''sqlalchemy connect'''
+    '''sqlalchemy connect '''
     connstr = 'oracle://{user}:{password}@{sid}'.format(
         user=db_informations['username'],
         password=db_informations['passwd'],
         sid=oracle_tns)
     db = sqlalchemy.create_engine(connstr, echo=False)  # true will echo executing sql
-    return conn, db
+    return conn,db
